ef963fcdf33364b449fc53a94d6711c53a618e7a
hana <dlagksk64@naver.com>
2021-06-14 20:29:57 +0900

    script basic02 - function

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,42 +1,27 @@
-let testValue = 1;
-console.log(typeof testValue);
-const testValue2 = 1.2;
-console.log(typeof testValue2)
-let testSymbol1 = Symbol(1);
-let testSymbol2 = Symbol(1);
-console.log(typeof testSymbol1, testSymbol1);
-console.log(typeof testSymbol2, testSymbol2);
-console.log(testSymbol1 == testSymbol2);
-console.log(testSymbol1 === testSymbol2);
-let ttValue = "1";
-console.log(typeof ttValue, Number(ttValue));
-console.log(typeof parseInt(ttValue), Number(ttValue));
-console.log(typeof String(ttValue), Number(ttValue));
-console.log(typeof Boolean(0), Boolean(0));
-console.log((100 + 1) * 3);
-let value1 = 1;
-console.log(value1++ === 2);
-console.log(++value1 === 3);
-let age = 19;
-if (age <= 19) {
-    console.log('성인입니다');
-} else if (age >= 15) {
-    console.log('청소년입니다');
-} else {
-    console.log('어린이입니다');
+function FunCoding(name) {
+    console.log(name);
 }
+FunCoding('hana');
 
-let age2 = 15;
-switch (age2) {
-    case 12:
-        console.log('12살입니다');
-        break;
-    case 13:
-        console.log('13살입니다');
-        break;
-    case 14:
-        console.log('14살입니다');
-        break;
-    default:
-        console.log('매칭되는 나이가 없습니다.');
+function FunCoding2(name) {
+    return 'Hi! ' + name;
 }
+console.log(FunCoding2('Hana'));
+
+let func = (name) => console.log(name + ' yes!');
+func('Hana');
+
+let f1 = () => "Hello !!";
+f1();
+
+let f2 = age => age * 2;
+console.log(f2(1));
+f2();
+
+let f3 = (item1, item2) => {
+    return item1 * item2;
+}
+console.log(f3(1, 3));
+
+let f3_1 = (item1, item2) => item1 * item2;
+console.log(f3_1(1, 4));
