9236decf58983a6b75d2bc379d307ff8000f4a84
hana <dlagksk64@naver.com>
2021-06-16 00:57:20 +0900

    hoisting

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,42 +1,12 @@
-let a = 1;
-let b = 2;
+console.log(a);
+a = 10;
+console.log(a);
+var a = 20;
+console.log(a);
 
-[a, b] = [b, a];
-console.log(a, b);
+getData();
 
 function getData() {
-    return [10, 20];
+    console.log('Hello!!');
 }
 
-let [c, d, e] = getData();
-console.log(c, d, e);
-
-let data = 'Hana Im, fun-coding, coding';
-let [name, brand, hobby] = data.split(',');
-
-console.log(data.split(','));
-console.log(name, brand);
-
-
-function getData2(a, b, ...rest) {
-    console.log(rest);
-}
-
-getData2(1, 2, 3, 4, 5);
-
-const data3 = [1, 2, 3];
-
-console.log(data3);
-console.log(...data3);
-
-function func(a, b, c, d) {
-    console.log(b, c);
-}
-
-const data4 = [1, 2, 3, 4];
-func(...data4);
-
-const data01 = [1, 2, 3];
-const data02 = [0, ...data01, 4, 5];
-
-console.log(data02);
