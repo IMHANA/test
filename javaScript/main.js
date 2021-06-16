379de0b1fabf41aa59316206ba957ae8f5bf54cf
hana <dlagksk64@naver.com>
2021-06-16 17:53:44 +0900

    promise (Syncronous / Asynchronous)

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,12 +1,20 @@
-console.log(a);
-a = 10;
-console.log(a);
-var a = 20;
-console.log(a);
+console.log('안녕안녕');
+setTimeout(() => {
+    console.log('출력해주세오');
+}, 3000);
+console.log('Hana');
 
-getData();
 
-function getData() {
-    console.log('Hello!!');
+console.log('안녕안녕');
+function desc(callback) {
+    setTimeout(() => {
+    console.log('하나입니다.');
+    callback();
+    }, 3000);
 }
 
+function desc2() {
+    console.log('둘입니다.');
+}
+
+desc(desc2);
